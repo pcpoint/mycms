9733b144e332220eee7297ec934c33c47f47c3c9
sherwali <sherwal@gmail.com>
2019-12-24 10:01:14 +0500

    after comipiling css only

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+   'resources/assets/css/blog-post.css',
+   'resources/assets/css/metisMenu.css'
+], 'public/css/app.css');
