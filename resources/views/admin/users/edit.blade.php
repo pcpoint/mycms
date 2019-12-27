771c4834c55be2103fce2ef2bc6211df660a3695
sherwali <sherwal@gmail.com>
2019-12-27 14:51:01 +0500

    users working

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Edit User</h1>
+
+<div class="row">
+
+<div class="col-sm-2">
+    <img height="300" src="{{$user->photo ? $user->photo->file : 'https://placehold.co/300'}}" alt="" class="img-responsive img-rounded">
+    </div>
+
+<div class="col-sm-9">
+   
+
+{!! Form::model($user, ['method'=>'PATCH' ,'action'=>['AdminUsersController@update',$user->id], 'files'=>'true'])!!}
+
+<div class = "form-group px-5">
+    {!! Form::label('name', 'Name:' ) !!}
+    {!! Form::text('name', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class = "form-group px-5">
+    {!! Form::label('email ', 'Email:' ) !!}
+    {!! Form::text('email', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class = "form-group px-5">
+    {!! Form::label('role_id', 'Role:') !!}
+    {!! Form::select('role_id', [''=>'Choose Option'] +  $roles, null, ['class'=>'form-control']) !!}
+    
+</div>
+
+<div class = "form-group px-5">
+    {!! Form::label('is_active', 'Status:' ) !!}
+    {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), null, ['class'=>'form-control']) !!}
+</div>
+
+<div class = "form-group px-5">
+    {!! Form::label('photo_id', 'Photo:' ) !!}
+    {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class = "form-group px-5">
+    {!! Form::label('password', 'Password:' ) !!}
+    {!! Form::password('password',['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group px-5">
+{!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+
+</div>
+{!! Form::close() !!}
+</div>
+</div>
+<div>@include('/includes.form_errors')
+</div>
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,6 +7,7 @@
     <thead>
       <tr>
         <th>ID</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -24,7 +25,8 @@
 
       <tr>
         <td>{{$user->id}}</td>
-        <td>{{$user->name}}</td>
+      <td> <img height="50" src="{{$user->photo ? $user->photo->file : 'https://placehold.co/50'}}" alt="" class="img-responsive img-rounded"></td>
+      <td><a href="{{route('users.edit', $user->id)}}">{{$user->name}}</a></td>
         <td>{{$user->email}}</td>
          <td>{{$user->role['name']}}</td>
         <td>{{$user->is_active==1 ? 'Active' : 'Not Active'}}
